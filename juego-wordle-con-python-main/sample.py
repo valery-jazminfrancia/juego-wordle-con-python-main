2e578a750a8aae7c93de566b40d0e25be6a99937
francia paucar <134339070+valery-jazminfrancia@users.noreply.github.com>
2023-08-23 17:19:25 -0500

    Add files via upload

---
+++ juego-wordle-con-python-main/sample.py
@@ -0,0 +1,18 @@
+
+importar matematicas
+de importación de tortugas *
+def  hearta ( k ):
+    devuelve  15 * matemáticas. pecado ( k ) ** 3
+def  heartb ( k ):
+    devuelve  12 * matemáticas. porque ( k ) - 5 * \
+    matemáticas. porque ( 2 * k ) - 2 * \
+    matemáticas. porque ( 3 * k ) - \
+    matemáticas. porque ( 4 * k )
+velocidad ( 0 )
+bgcolor ( "negro" )
+para  i  en el  rango ( 6000 ):
+    ir a ( corazón ( i ) * 20 , corazónb ( i ) * 20 )
+    para  j  en el  rango ( 5 ):
+        color ( "rojo" )
+    ir a ( 0 , 0 )
+hecho ()
